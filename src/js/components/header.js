80fce9ce6bc8bd9e7c77ade21d998c7320fa4819
Thomas Dailey <daileytj89@gmail.com>
2017-05-30 18:53:02 -0400

    update/fix routing

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,31 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import * as actions from '../actions/index';
+// import * as actions from '../actions/index';
 // import clashRoyaleLogo from '../../images/clash-royale-logo.png';
 // <img src = {clashRoyaleLogo} alt = "Clash Royale Logo" />
 
 class Header extends React.Component {
-    constructor(props){
-      super(props);
-      this.sendStateToBuild = this.sendStateToBuild.bind(this);
-      this.sendStateToDecks = this.sendStateToDecks.bind(this);
-      this.sendStateToCards = this.sendStateToCards.bind(this);
-    }
-
-    sendStateToBuild(){
-        this.props.dispatch(actions.sendStateToBuild());
-    }
-
-    sendStateToDecks(){
-        this.props.dispatch(actions.sendStateToDecks());
-    }
-
-    sendStateToCards(){
-        this.props.dispatch(actions.sendStateToCards());
-    }
-
-
     render() {
       return (
           <nav>
@@ -35,9 +15,9 @@ class Header extends React.Component {
             </Link></li>
         <img className = "hamburger-icon" src = "http://liquidcc.com/wp-content/uploads/2015/02/crown.png" alt = "Nav Toggle Icon"/>
             <ul className = "nav-wrapper" >
-              <li><Link onClick = {this.sendStateToBuild} to = "/build" className = "nav-link">BUILD</Link></li>
-              <li><Link onClick = {this.sendStateToDecks} to = "/decks" className = "nav-link">DECKS</Link></li>
-              <li><Link onClick = {this.sendStateToCards} to = "/cards" className = "nav-link">ALL CARDS</Link></li>
+              <li><Link to = "/build" className = "nav-link">BUILD</Link></li>
+              <li><Link to = "/decks" className = "nav-link">DECKS</Link></li>
+              <li><Link to = "/cards" className = "nav-link">ALL CARDS</Link></li>
             </ul>
           </nav>
         );
