0b9d6c682d181159514d9b4a3ca1bb51c9be2791
Thomas Dailey <daileytj89@gmail.com>
2017-05-30 17:08:26 -0400

    update github link

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -10,7 +10,7 @@ class Footer extends React.Component{
   render() {
     return (
       <footer>
-        <a href = "https://github.com/daileytj/clash_royale_deck_builder" target = "_blank" rel="noopener noreferrer"><img src = {githubLogo} alt = "GitHub Logo" /></a>
+        <a href = "https://github.com/daileytj/clash_royale_deck_builder_app" target = "_blank" rel="noopener noreferrer"><img src = {githubLogo} alt = "GitHub Logo" /></a>
       </footer>
     )
   }
