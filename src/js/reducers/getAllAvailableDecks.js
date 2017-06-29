66162749db50c53c7576f343d99c5e1f6e033094
Thomas Dailey <daileytj89@gmail.com>
2017-06-29 16:47:41 -0400

    update reducer to manage state for decks

--- src/js/reducers/getAllAvailableDecks.js
+++ src/js/reducers/getAllAvailableDecks.js
@@ -1,6 +1,17 @@
-const getAllAvailableDecks=(state=[], action) => {
-  // console.log('getting all available decks');
-  return state;
+const initialState={
+  cards: []
+}
+
+const getAllAvailableDecks=(state=initialState, action) => {
+  switch (action.type) {
+    case 'GET_ALL_AVAILABLE_DECKS_SUCCESS':
+      return {
+        ...state,
+        decks: action.response
+      }
+    default:
+      return state;
+  }
 }
 
 export default getAllAvailableDecks;
