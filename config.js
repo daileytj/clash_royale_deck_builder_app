8b114e4c08dbf24d0ebd9e196eda3b509e852bd3
Thomas Dailey <daileytj89@gmail.com>
2017-07-03 17:17:48 -0400

    server update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL=process.env.DATABASE_URL ||
     global.DATABASE_URL ||
-    'mongodb://test:test@ds127801.mlab.com:27801/clash_royale_decks';
+    'mongodb://localhost/clash_royale';
 exports.PORT=process.env.PORT || 8080;
